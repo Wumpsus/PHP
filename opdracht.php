1df7af29ad1480da860a5092ec3283ba2f1b6735
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-11-17 13:31:14 +0100

    e

--- opdracht.php
+++ opdracht.php
@@ -7,13 +7,23 @@
 
   //main
 
-  echo "Datum is $datum.";
+  echo "datum is: $datum";
+
   echo "<br>";
-  echo "Vandaag is het de " . date("z") . "e dag van het jaar.";
+  echo "vandaag is het de: " . date("z") . "e dag van het jaar";
   echo "<br>";
-  echo date("l") . " is de " . date("w") . "e dag van de week.";
+  echo date("l") . " is de " . date("w") . "e dag van de week";
   echo "<br>";
-  echo "De maand " . date("F") . "heeft in totaal " . date("t") . " dagen.";
+  echo "de maand " . date("F") . " heeft in totaal " . date("t") . " dagen";
   echo "<br>";
-  echo "Het jaar " . date("Y") . " is geen " . date("L");
+  echo "het jaar " . date("Y");
+  $t = date("L");
+  if ($t < "0") {
+echo " is geen schrikkeljaar";
+}
+
+  if ($t < "1") {
+echo " is een schrikkeljaar";
+}
+
 ?>
