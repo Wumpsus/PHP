862ab4458a32bc7098049579ddd4c4cf1c42c26d
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-11-22 12:14:07 +0100

    e

---
+++ Opdracht 3.php
@@ -0,0 +1,19 @@
+<?php
+  //Auteur: Michael Davelaar
+  //Functie: Strings yippie
+
+  // Main
+
+  $yippie[1] = "String 1";
+  $yippie[2] = "String 2";
+  $yippie[3] = "1";
+  $yippie[4] = "2";
+
+  echo $yippie[1];
+  echo "<br>";
+  echo $yippie[2];
+  echo "<br>";
+  echo $yippie[3];
+  echo "<br>";
+  echo $yippie[4];
+?>
