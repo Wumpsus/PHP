7500bbff5062d01a2ac5eba037159e8471033a69
Wumpus <michaeldavelaaryoshi@gmail.com>
2025-03-18 08:47:01 +0100

    logout vergeten te adden oopsie

--- Leerjaar 2/Code voor de opdracht/login_form.php
+++ Leerjaar 2/Code voor de opdracht/login_form.php
@@ -49,23 +49,25 @@
 	</head>
 <body>
 
-	<h3>PHP - PDO Login and Registration</h3>
-	<hr/>
-	
-	<form action="" method="POST">	
-		<h4>Login here...</h4>
-		<hr>
-		
-		<label>Username</label>
-		<input type="text" name="username" />
-		<br>
-		<label>Password</label>
-		<input type="password" name="password" />
-		<br>
-		<button type="submit" name="login-btn">Login</button>
-		<br>
-		<a href="register_form.php">Registration</a>
-	</form>
+<h3>PHP - PDO Login and Registration</h3>
+    <hr/>
+    
+    <form action="" method="POST">	
+        <h4>Login here...</h4>
+        <hr>
+        
+        <label>Username</label>
+        <input type="text" name="username" />
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" />
+        <br>
+        <button type="submit" name="login-btn">Login</button>
+        <br>
+        <a href="register_form.php">Registration</a>
+        <br>
+        <a href="logout.php">Logout</a>
+    </form>
 		
 </body>
 </html>
